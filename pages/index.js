b1c8431c39c9d316e9a3af3ca6accbd092364a90
Thales Garcia <89078112+Thalesgcostta@users.noreply.github.com>
2023-09-17 12:06:43 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Tabuleiro from '../components/Tabuleiro';
+
+
+const Home = () => {
+  return (
+    <div>
+  <center><h1>Tabuleiro de Damas</h1></center>
+      <Tabuleiro />
+    </div>
+    
+  );
+  
+};
+export default Home;
+
+
